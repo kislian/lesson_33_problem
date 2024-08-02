2338a9f2e0fc60c2ba52f139fa7e41d437a0c905
kislian <kisli_an@mail.ru>
2024-08-02 16:51:09 +0800

    lesson33

---
+++ src/main/java/pro/sky/demo/Lesson33Application.java
@@ -0,0 +1,13 @@
+package pro.sky.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson33Application.class, args);
+	}
+
+}
